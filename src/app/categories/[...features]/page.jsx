5a67de2bab00fa9d4d1f80496b2759e21976fd65
devmahin <mahinhowlader123@gmail.com>
2024-06-30 01:13:43 +0600

    daynamic multipel page completed

---
+++ src/app/categories/[...features]/page.jsx
@@ -0,0 +1,13 @@
+function CategoriesDetailspage({ params }) {
+    console.log("Length",params.features.length)
+    console.log("params.features[2]", params.features[2])
+    if (params.features.length == 3) return <div>{params.features[2]}</div>
+    if (params.features.length == 2) return <div>{params.features[1]}</div>
+    return (
+        <div>
+            Categories details
+        </div>
+    );
+}
+
+export default CategoriesDetailspage;
---
+++ src/app/categories/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function page(props) {
+    return (
+        <div>
+            category
+        </div>
+    );
+}
+
+export default page;
